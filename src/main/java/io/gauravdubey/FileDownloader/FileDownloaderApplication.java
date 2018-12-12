ad1012677c297d7e25243c2f2194cc2b1c059ea5
B0208062 <b0208062@LTB0208062-MAC.local>
2018-12-12 12:40:30 +0530

    setting up spring project from  http://start.spring.io

---
+++ src/main/java/io/gauravdubey/FileDownloader/FileDownloaderApplication.java
@@ -0,0 +1,12 @@
+package io.gauravdubey.FileDownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDownloaderApplication.class, args);
+	}
+}
